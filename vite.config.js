a27da13bf43bb63c294310b3ec107e78738c8c05
cepiseptiyana <cepifams3@gmail.com>
2025-03-15 15:26:19 +0700

    base path :/

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  // base: '/',
   // base: '/portfolioCepiSeptiyana/',
   plugins: [
     vue(),
